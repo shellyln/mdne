e33af9df21b84c0e06f770852d7861b5cee4278e
shellyln <shellyln@users.noreply.github.com>
2019-06-22 19:00:58 +0900

    provide enhancing method to add macros on web-app mode.

--- contents/assets/script/backend-emu.js
+++ contents/assets/script/backend-emu.js
@@ -13,10 +13,15 @@
     // eslint-disable-next-line no-unused-vars
     window.renderByMenneu = window.renderByMenneu || (async (source, data, options, srcPath, ...exportPath) => {
         const opts = Object.assign({}, options, {
+            // NOTE: to enhance macros, define Vx02PGUB3NFWwhsd__replacementMacros.
             replacementMacros: [{
                 re: /!!!lsx\s([\s\S]+?)!!!/g,
                 fn: 'lsx', // evaluate input as LSX script
-            }],
+            }].concat(
+                window.Vx02PGUB3NFWwhsd__replacementMacros &&
+                Array.isArray(window.Vx02PGUB3NFWwhsd__replacementMacros) ?
+                    window.Vx02PGUB3NFWwhsd__replacementMacros:
+                    []),
         });
         if (!opts.outputFormat || opts.outputFormat.toLowerCase() !== 'html') {
             const errText = `output format ${opts.outputFormat} is not available.`;
