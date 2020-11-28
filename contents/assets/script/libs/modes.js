2c24a4cce38bd04e59db6678d403505d92cba245
shellyln <shellyln@users.noreply.github.com>
2020-11-29 08:10:47 +0900

    Update contents dependencies

--- contents/assets/script/libs/modes.js
+++ contents/assets/script/libs/modes.js
@@ -4,11 +4,23 @@
 
 
 
+/**
+ * @param {string} path
+ * @return {string}
+ */
 export function getInputFormat(path) {
     switch (path.lastIndexOf('.') >= 0 ?
         path.toLowerCase().slice(path.lastIndexOf('.')) : '') {
     case '.md': case '.markdown':
         return 'md';
+    case '.html': case '.htm':
+        return 'html';
+    case '.lisp': case '.lsx':
+        return 'lisp';
+    case '.tex':
+        return 'tex';
+    case '.latex':
+        return 'latex';
     case '.css':
         return 'css';
     case '.less':
@@ -17,42 +29,64 @@ export function getInputFormat(path) {
         return 'sass';
     case '.scss':
         return 'scss';
-    case '.html': case '.htm':
-        return 'html';
-    case '.lisp': case '.lsx':
-        return 'lisp';
     case '.json':
         return 'json';
+    case '.json5':
+        return 'json5';
     case '.js': case '.mjs': case '.cjs':
         return 'js';
+    case '.jsx':
+        return 'jsx';
     case '.ts':
         return 'ts';
-    case '.jsx': case '.tsx':
+    case '.tsx':
         return 'tsx';
     case '.svg':
         return 'svg';
     case '.xml':
         return 'xml';
-    case '.sh':
-        return 'sh';
     case '.yaml': case '.yml':
         return 'yaml';
+    case '.graphql': case '.gql':
+        return 'graphql';
     case '.proto': case '.proto2': case '.proto3':
         return 'protobuf';
     case '.sql':
         return 'sql';
-    case '.graphql': case '.gql':
-        return 'graphql';
     case '.cs':
         return 'csharp';
+    case '.go':
+        return 'golang';
+    case '.rb':
+        return 'ruby';
+    case '.rs':
+        return 'rust';
     case '.py':
         return 'python';
+    case '.r':
+        return 'r';
+    case '.sh':
+        return 'sh';
+    case '.dockerfile':
+        return 'dockerfile';
+    case '.makefile':
+        return 'makefile';
     default:
+        if (path.toLowerCase().endsWith('dockerfile')) {
+            return 'dockerfile';
+        }
+        if (path.toLowerCase().endsWith('makefile')) {
+            return 'makefile';
+        }
         return 'text';
     }
 }
 
 
+/**
+ * @param {string} inputFormat
+ * @return {boolean}
+ */
 export function isPreviewable(inputFormat) {
     switch (inputFormat) {
     case 'md':
@@ -67,10 +101,22 @@ export function isPreviewable(inputFormat) {
 }
 
 
+/**
+ * @param {string} inputFormat
+ * @return {string | undefined}
+ */
 export function getAceEditorMode(inputFormat) {
     switch (inputFormat) {
     case 'md':
         return 'ace/mode/markdown';
+    case 'html':
+        return 'ace/mode/html';
+    case 'lisp':
+        return 'ace/mode/lisp';
+    case 'tex':
+        return 'ace/mode/tex';
+    case 'latex':
+        return 'ace/mode/latex';
     case 'css':
         return 'ace/mode/css';
     case 'less':
@@ -79,14 +125,14 @@ export function getAceEditorMode(inputFormat) {
         return 'ace/mode/sass';
     case 'scss':
         return 'ace/mode/scss';
-    case 'html':
-        return 'ace/mode/html';
-    case 'lisp':
-        return 'ace/mode/lisp';
     case 'json':
         return 'ace/mode/json';
+    case 'json5':
+        return 'ace/mode/json5';
     case 'js':
         return 'ace/mode/javascript';
+    case 'jsx':
+        return 'ace/mode/jsx';
     case 'ts':
         return 'ace/mode/typescript';
     case 'tsx':
@@ -95,20 +141,32 @@ export function getAceEditorMode(inputFormat) {
         return 'ace/mode/svg';
     case 'xml':
         return 'ace/mode/xml';
-    case 'sh':
-        return 'ace/mode/sh';
     case 'yaml':
         return 'ace/mode/yaml';
+    case 'graphql':
+        return 'ace/mode/graphqlshema';
     case 'protobuf':
         return 'ace/mode/protobuf';
     case 'sql':
         return 'ace/mode/sql';
-    case 'graphql':
-        return 'ace/mode/graphqlshema';
     case 'csharp':
         return 'ace/mode/csharp';
+    case 'golang':
+        return 'ace/mode/golang';
+    case 'ruby':
+        return 'ace/mode/ruby';
+    case 'rust':
+        return 'ace/mode/rust';
     case 'python':
         return 'ace/mode/python';
+    case 'r':
+        return 'ace/mode/r';
+    case 'sh':
+        return 'ace/mode/sh';
+    case 'dockerfile':
+        return 'ace/mode/dockerfile';
+    case 'makefile':
+        return 'ace/mode/makefile';
     case 'text':
         return 'ace/mode/text';
     default:
--- contents/service-worker.js
+++ contents/service-worker.js
@@ -16,7 +16,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  `./precache-manifest.${'e3e1de38f081b669545e7609ea12d643'}.js`
+  `./precache-manifest.${'425ae09754945d71f6967738bdfa0cbc'}.js`
 );
 
 self.addEventListener('message', (event) => {
